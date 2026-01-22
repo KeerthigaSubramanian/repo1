de6153dcc8e07c4538460367c139d872454d45c3
KeerthigaSubramanian <dummy@gmail.com>
2026-01-22 04:21:05 +0000

    Day 6 Task

--- server/src/main/java/com/edutech/progressive/entity/Patient.java
+++ server/src/main/java/com/edutech/progressive/entity/Patient.java
@@ -2,8 +2,19 @@ package com.edutech.progressive.entity;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Patient")
 public class Patient implements Comparable<Patient>{
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int patientId;
     private String fullName;
     private Date dateOfBirth;
